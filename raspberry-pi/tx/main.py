dd831ff496532f180054f9b8c570571cef8cf3ed
Mason Jacob <masonjacob@uri.edu>
2024-08-15 14:25:38 -0400

    add double integral calculation

--- raspberry-pi/tx/main.py
+++ raspberry-pi/tx/main.py
@@ -1,32 +1,48 @@
 import RPi.GPIO as gpio
 from time import sleep
-import math
+import numpy as np
+from scipy.integrate import quad, dblquad
 
-G_H1 = 2
-G_L1 = 3
-G_H2 = 4
-G_L2 = 17 
+N_1 = 100
+N_2 = 100
+μ_0 = 0.1
+r_1 = 0.1
+r_2 = 0.1
+c = 0.1
+d = 0.1
+alpha = 0
+theta = 0.5
+omega = 0.5
+func = (r_1 * r_2  * ((np.sin(theta) * np.sin(omega)) + (np.cos(alpha) * np.cos(theta) * np.cos(omega))) / np.sqrt((r_1 * np.cos(theta) - r_2 * np.cos(omega))^2 + (r_1 * np.sin(theta) - r_2 * np.sin(omega) * np.cos(alpha) - c)^2 +(r_2 * np.sin(omega) * np.sin(alpha) - d)^2))
+coefficent = (N_1 * N_2 * μ_0) / (4 * np.pi)
+print(dblquad(lambda theta, omega: func , 0, 2 * np.pi, lambda theta: 0, lambda theta: 2 * np.pi))
 
-gpio.setmode(gpio.BCM)
-gpio.setup(G_H1, gpio.OUT)
-gpio.setup(G_L1, gpio.OUT)
-gpio.setup(G_H2, gpio.OUT)
-gpio.setup(G_L2, gpio.OUT)
+# G_H1 = 2
+# G_L1 = 3
+# G_H2 = 4
+# G_L2 = 17 
 
-frequency = 10000  # 100kHz
+# gpio.setmode(gpio.BCM)
+# gpio.setup(G_H1, gpio.OUT)
+# gpio.setup(G_L1, gpio.OUT)
+# gpio.setup(G_H2, gpio.OUT)
+# gpio.setup(G_L2, gpio.OUT)
 
-try:
-    while True:
-        gpio.output(G_H1, gpio.HIGH)
-        gpio.output(G_L2, gpio.HIGH)
-        gpio.output(G_H2, gpio.LOW)
-        gpio.output(G_L1, gpio.LOW)
-        sleep(0.5 / frequency)
-        gpio.output(G_H1, gpio.LOW)
-        gpio.output(G_L2, gpio.LOW)
-        gpio.output(G_H2, gpio.HIGH)
-        gpio.output(G_L1, gpio.HIGH)
-        sleep(0.5 / frequency)
+# frequency = 10000  # 100kHz
 
-except KeyboardInterrupt:
-    gpio.cleanup()
+# try:
+#     while True:
+#         gpio.output(G_H1, gpio.HIGH)
+#         gpio.output(G_L2, gpio.HIGH)
+#         gpio.output(G_H2, gpio.LOW)
+#         gpio.output(G_L1, gpio.LOW)
+#         sleep(0.5 / frequency)
+#         gpio.output(G_H1, gpio.LOW)
+#         gpio.output(G_L2, gpio.LOW)
+#         gpio.output(G_H2, gpio.HIGH)
+#         gpio.output(G_L1, gpio.HIGH)
+#         sleep(0.5 / frequency)
+
+# except KeyboardInterrupt:
+#     gpio.cleanup()
+
